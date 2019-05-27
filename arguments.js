803008da6928ca721fa66ac71702faec86f0f315
Olha Bahaieva <leila_1605@yahoo.com>
2019-05-27 18:32:53 +0300

    Update arguments.js

--- arguments.js
+++ arguments.js
@@ -8,7 +8,6 @@ let result = add(10, 1, 5)
 console.log(result)
 
 
-
 //Default arguments
 let getScroreText = function(name = 'Anonymous', score = 0){
     return 'Name: ' + name + ' Score: ' + score
